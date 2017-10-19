02c10d86cdfc36f43e5cede28f2f8f5285d961d4
cedretaber <cedretaber@gmail.com>
2017-10-19 17:58:05 +0900

    Fix code style.

--- exercise/src/exercise/core.cljs
+++ exercise/src/exercise/core.cljs
@@ -1,7 +1,6 @@
 (ns exercise.core
-    (:require
-      [reagent.core :as r]
-      [exercise.home :as home]))
+  (:require [reagent.core :as r]
+            [exercise.home :as home]))
 
 (defn mount-root []
   (r/render [home/home-page] (.getElementById js/document "app")))
